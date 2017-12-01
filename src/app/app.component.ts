58bcf0e0c7390a42201f4f33a578d41946a525c9
Jasper Cao <jaspersitacorp@gmail.com>
2017-12-01 14:06:45 -0500

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  showContent = false;
+  log = [];
+
+  onDisplayContent() {
+    this.showContent = !this.showContent;
+    this.log.push(this.log.length + 1);
+  }
+
+
+}
+
